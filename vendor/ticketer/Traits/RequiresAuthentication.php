6ef85b452f23f2493c2777a4800158745cf28517
Jack Polgar <nrx@nirix.net>
2012-12-03 16:45:56 +1100

    Added the Requiresuthentication trait.

---
+++ vendor/ticketer/Traits/RequiresAuthentication.php
@@ -0,0 +1,54 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Traits;
+
+use Radium\Output\View;
+
+/**
+ * Requires Authentication on controllers
+ * that use this trait.
+ *
+ * @package Ticketer
+ * @subpackage Traits
+ */
+trait RequiresAuthentication
+{
+    /**
+     * Add the filter.
+     */
+    public function filters()
+    {
+        parent::filters();
+        $this->before['*'][] = 'requireAuthentication';
+    }
+
+    /**
+     * Checks if the user is logged in.
+     */
+    public function requireAuthentication()
+    {
+        if (!LOGGEDIN) {
+            $this->render['view'] = false;
+            View::render('users/login');
+        }
+    }
+}
