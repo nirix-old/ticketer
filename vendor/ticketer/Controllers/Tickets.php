7f29e9c2cd8158b6567b79a584ddae087c296991
Jack Polgar <nrx@nirix.net>
2012-12-03 16:46:12 +1100

    Ticket controller started.

---
+++ vendor/ticketer/Controllers/Tickets.php
@@ -0,0 +1,44 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Controllers;
+
+use Ticketer\Models\Ticket;
+
+/**
+ * Ticket controller.
+ *
+ * @package Ticketer
+ * @subpackage Controllers
+ */
+class Tickets extends AppController
+{
+    use \Ticketer\Traits\RequiresAuthentication;
+
+    /**
+     * Users ticket listing page.
+     */
+    public function indexAction()
+    {
+        $tickets = Ticket::select()->where('user_id = ?', $this->currentUser->id)->fetchAll();
+        $this->set(compact('tickets'));
+    }
+}
