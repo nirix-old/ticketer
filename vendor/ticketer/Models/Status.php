4e430a6929dae7fdce883522565bda5eec98349a
Jack Polgar <nrx@nirix.net>
2012-12-05 06:47:20 +1100

    Added Status model.

---
+++ vendor/ticketer/Models/Status.php
@@ -0,0 +1,44 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Models;
+
+/**
+ * Status model.
+ *
+ * @package Ticketer
+ * @subpackage Models
+ */
+class Status extends \Radium\Database\Model
+{
+    protected static $_table = 'statuses';
+
+    public static function selectOptions()
+    {
+        $options = [];
+
+        foreach (static::all() as $row) {
+            $options[] = ['label' => $row->name, 'value' => $row->id];
+        }
+
+        return $options;
+    }
+}
