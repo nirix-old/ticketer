f205baf306a493b4b9a4c0b81b2bf5ea7044303a
Jack Polgar <nrx@nirix.net>
2012-12-04 23:57:16 +1100

    Added example database config file.

---
+++ config/database.new.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'driver'   => "pdo",
+    'type'     => "mysql",
+    'host'     => "localhost",
+    'username' => "root",
+    'password' => "",
+    'database' => "ticketer"
+];
