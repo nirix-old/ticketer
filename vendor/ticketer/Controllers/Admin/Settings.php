b8705993d985b48d58b6b508b2cacd03e3b4c768
Jack Polgar <nrx@nirix.net>
2012-12-09 18:55:59 +1100

    Settings page started.

---
+++ vendor/ticketer/Controllers/Admin/Settings.php
@@ -0,0 +1,38 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Controllers\Admin;
+
+use Ticketer\Models\Ticket;
+use Ticketer\Models\User;
+
+/**
+ * Settings controller.
+ *
+ * @package Ticketer
+ * @subpackage Controllers
+ */
+class Settings extends AppController
+{
+    public function indexAction()
+    {
+    }
+}
