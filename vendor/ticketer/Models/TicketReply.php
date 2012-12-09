4c93485c5e960fa249bffd8b912ce5558c852101
Jack Polgar <nrx@nirix.net>
2012-12-09 15:41:21 +1100

    Added the TicketReply model.

---
+++ vendor/ticketer/Models/TicketReply.php
@@ -0,0 +1,35 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Models;
+
+/**
+ * TicketReply model.
+ *
+ * @package Ticketer
+ * @subpackage Models
+ */
+class TicketReply extends \Radium\Database\Model
+{
+    protected static $_table = 'ticket_replies';
+
+    protected static $_belongsTo = ['user'];
+}
