a5b1ce49d6bcf058b7ef073b9dee2b5a34e32129
Jack Polgar <nrx@nirix.net>
2012-12-09 23:44:40 +1100

    Status listing page done.

---
+++ vendor/ticketer/Controllers/Admin/Statuses.php
@@ -0,0 +1,42 @@
+<?php
+/*!
+ * Ticketer
+ * Copyright (C) 2012 Jack P.
+ * https://github.com/nirix
+ *
+ * This file is part of Ticketer.
+ *
+ * Ticketer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Ticketer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ticketer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ticketer\Controllers\Admin;
+
+use Radium\Http\Request;
+use Ticketer\Models\Status;
+
+/**
+ * Statuses controller.
+ *
+ * @package Ticketer
+ * @subpackage Controllers
+ */
+class Statuses extends AppController
+{
+    /**
+     * Status listing.
+     */
+    public function indexAction()
+    {
+        $this->set('statuses', Status::all());
+    }
+}
